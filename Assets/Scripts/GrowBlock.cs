b89c89a5c8e676cea8558aa0350679417f7ac762
Tristanto Wijaya <tristantowijaya168@gmail.com>
2025-04-13 15:34:07 +0700

    Initial commit on tools update

---
+++ Assets/Scripts/GrowBlock.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GrowBlock : MonoBehaviour
+{
+
+    public enum GrowthStage
+    {
+        barren,
+        ploughed,
+        planted,
+        growing1,
+        growing2,
+        ripe
+    }
+
+    public GrowthStage currentStage;
+
+    public SpriteRenderer theSR;
+    public Sprite soilTilled;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        /*
+        if(Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            AdvanceStage();
+
+            SetSoilSprite();
+        }
+        */
+    }
+
+    public void AdvanceStage()
+    {
+        currentStage = currentStage + 1;
+
+        if((int)currentStage >= 6)
+        {
+            currentStage = GrowthStage.barren;
+        }
+    }
+
+    public void SetSoilSprite()
+    {
+        if(currentStage == GrowthStage.barren)
+        {
+            theSR.sprite = null;
+        }
+
+        else
+        {
+            theSR.sprite = soilTilled;
+        }
+    
+    }
+
+    public void PloughSoil()
+    {
+        if(currentStage == GrowthStage.barren)
+        {
+            currentStage = GrowthStage.ploughed;
+
+            SetSoilSprite();
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,136 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D theRB;
+    public float moveSpeed;
+
+    public InputActionReference moveInput, actionInput;
+    public Animator anim;
+
+    public enum ToolType
+    {
+        plough,
+        wateringCan,
+        seeds,
+        basket
+    }
+
+    public ToolType currentTool;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        UIController.instance.SwitchTool((int)currentTool);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //theRB.linearVelocity = new Vector2(moveSpeed, 0f);
+        theRB.linearVelocity = moveInput.action.ReadValue<Vector2>().normalized * moveSpeed;
+
+
+        if(theRB.linearVelocity.x < 0f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if(theRB.linearVelocity.x > 0f)
+        {
+            transform.localScale = Vector3.one;
+        }
+
+        bool hasSwitchedTool = false;
+
+        if(Keyboard.current.tabKey.wasPressedThisFrame)
+        {
+            currentTool++;
+
+            if((int)currentTool >= 4)
+            {
+                currentTool = ToolType.plough;
+                
+            }
+            hasSwitchedTool = true;
+        }
+
+        if(Keyboard.current.digit1Key.wasPressedThisFrame)
+        {
+            currentTool = ToolType.plough;
+            hasSwitchedTool = true;
+        }
+        if(Keyboard.current.digit2Key.wasPressedThisFrame)
+        {
+            currentTool = ToolType.wateringCan;
+            hasSwitchedTool = true;
+        }
+        if(Keyboard.current.digit3Key.wasPressedThisFrame)
+        {
+            currentTool = ToolType.seeds;
+            hasSwitchedTool = true;
+        }
+        if(Keyboard.current.digit4Key.wasPressedThisFrame)
+        {
+            currentTool = ToolType.basket;
+            hasSwitchedTool = true;
+        }
+
+        if(hasSwitchedTool == true)
+        {
+            //FindFirstObjectByType<UIController>().SwitchTool((int)currentTool);
+            UIController.instance.SwitchTool((int)currentTool);
+        }
+       
+
+
+
+        if(actionInput.action.WasPressedThisFrame())
+        {
+            UseTool();
+        }
+
+
+        anim.SetFloat("speed", theRB.linearVelocity.magnitude);
+    }
+
+    void UseTool()
+    {
+        GrowBlock block = null;
+
+        block = FindFirstObjectByType<GrowBlock>();
+
+        
+        //block.PloughSoil();
+
+        if(block != null)
+        {
+            switch(currentTool)
+            {
+                case ToolType.plough:
+                    
+                    block.PloughSoil();
+
+                    break;
+
+                case ToolType.wateringCan:
+
+
+                    break;
+                
+                case ToolType.seeds:
+
+
+                    break;
+
+                case ToolType.basket:
+
+
+                    break;
+
+                    
+            }
+        }
+    }
+
+}
