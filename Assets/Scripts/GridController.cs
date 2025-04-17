51722e000563e804fe772dc33fe2495a1c045be7
dexalium <64056693+dexalium@users.noreply.github.com>
2025-04-17 11:06:33 +0700

    Grid Manipulation Update

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,72 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    public Transform minPoint, maxPoint;
+
+    public GrowBlock baseGridBlock;
+
+    private Vector2Int gridSize;
+
+    public List<BlockRow> rows = new List<BlockRow>();
+
+    public List<GrowBlock> blocks = new List<GrowBlock>();
+
+    public LayerMask gridBlockers;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateGrid()
+    {
+        minPoint.position = new Vector3(Mathf.Round(minPoint.position.x), Mathf.Round(minPoint.position.y), 0f);
+        maxPoint.position = new Vector3(Mathf.Round(maxPoint.position.x), Mathf.Round(maxPoint.position.y), 0f);
+
+        Vector3 startpoint = minPoint.position + new Vector3(0.5f, 0.5f, 0f);
+
+        //Instantiate(baseGridBlock, startpoint, Quaternion.identity);
+
+        gridSize = new Vector2Int(
+            Mathf.RoundToInt(maxPoint.position.x - minPoint.position.x),
+            Mathf.RoundToInt(maxPoint.position.y - minPoint.position.y)
+        );
+
+        for(int y = 0; y < gridSize.y; y++)
+        {
+            rows.Add(new BlockRow());
+
+            for(int x = 0; x < gridSize.x; x++)
+            {
+                GrowBlock newBlock = Instantiate(baseGridBlock, startpoint + new Vector3(x, y, 0f), Quaternion.identity);
+
+                newBlock.transform.SetParent(transform);
+
+                rows[y].blocks.Add(newBlock);
+
+                if(Physics2D.OverlapBox(newBlock.transform.position, new Vector2(.9f, .9f), 0f, gridBlockers))
+                {
+                    newBlock.theSR.sprite = null;
+                    newBlock.preventUse = true;
+                }
+            }
+        }
+
+        baseGridBlock.gameObject.SetActive(false);
+    }
+}
+
+[System.Serializable]
+public class BlockRow
+{
+    public List<GrowBlock> blocks = new List<GrowBlock>();
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,21 +3,154 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D RigidBody;
+    public Rigidbody2D theRB;
     public float moveSpeed;
 
-    public InputActionReference moveInput;
+    public InputActionReference moveInput, actionInput;
+    public Animator anim;
+
+    public enum ToolType
+    {
+        plough,
+        wateringCan,
+        seeds,
+        basket
+    }
+
+    public ToolType currentTool;
+
+    public float toolWaitTime = .5f;
+    private float toolWaitCounter;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        UIController.instance.SwitchTool((int)currentTool);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //RigidBody.linearVelocity = new Vector2(moveSpeed, 0f);
-        RigidBody.linearVelocity = moveInput.action.ReadValue<Vector2>().normalized * moveSpeed;
+
+        if(toolWaitCounter > 0)
+        {
+            toolWaitCounter -= Time.deltaTime;
+            theRB.linearVelocity = Vector2.zero;
+        }
+        else
+    {
+            
+        //theRB.linearVelocity = new Vector2(moveSpeed, 0f);
+        theRB.linearVelocity = moveInput.action.ReadValue<Vector2>().normalized * moveSpeed;
+
+
+        if(theRB.linearVelocity.x < 0f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if(theRB.linearVelocity.x > 0f)
+        {
+            transform.localScale = Vector3.one;
+        }
     }
+
+        bool hasSwitchedTool = false;
+
+        if(Keyboard.current.tabKey.wasPressedThisFrame)
+        {
+            currentTool++;
+
+            if((int)currentTool >= 4)
+            {
+                currentTool = ToolType.plough;
+                
+            }
+            hasSwitchedTool = true;
+        }
+
+        if(Keyboard.current.digit1Key.wasPressedThisFrame)
+        {
+            currentTool = ToolType.plough;
+            hasSwitchedTool = true;
+        }
+        if(Keyboard.current.digit2Key.wasPressedThisFrame)
+        {
+            currentTool = ToolType.wateringCan;
+            hasSwitchedTool = true;
+        }
+        if(Keyboard.current.digit3Key.wasPressedThisFrame)
+        {
+            currentTool = ToolType.seeds;
+            hasSwitchedTool = true;
+        }
+        if(Keyboard.current.digit4Key.wasPressedThisFrame)
+        {
+            currentTool = ToolType.basket;
+            hasSwitchedTool = true;
+        }
+
+        if(hasSwitchedTool == true)
+        {
+            //FindFirstObjectByType<UIController>().SwitchTool((int)currentTool);
+            UIController.instance.SwitchTool((int)currentTool);
+        }
+       
+
+
+
+        if(actionInput.action.WasPressedThisFrame())
+        {
+            UseTool();
+        }
+
+
+        anim.SetFloat("speed", theRB.linearVelocity.magnitude);
+    }
+
+    void UseTool()
+    {
+        GrowBlock block = null;
+
+        block = FindFirstObjectByType<GrowBlock>();
+
+        
+        //block.PloughSoil();
+
+        toolWaitCounter = toolWaitTime;
+
+        if(block != null)
+        {
+            switch(currentTool)
+            {
+                case ToolType.plough:
+                    
+                    block.PloughSoil();
+
+                    anim.SetTrigger("usePlough");
+
+                    break;
+
+                case ToolType.wateringCan:
+
+                    block.WaterSoil();
+
+                    anim.SetTrigger("useWateringcan");
+              
+                    break;
+                
+                case ToolType.seeds:
+
+                    block.PlantCrop();
+                    
+                    break;
+
+                case ToolType.basket:
+                
+                    block.HarvestCrop();
+
+                    break;
+            }
+        }
+    }
+
 }
