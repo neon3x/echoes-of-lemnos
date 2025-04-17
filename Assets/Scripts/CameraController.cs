c209b99dee7e91f3e7cd08a4de330abc03a96941
dexalium <64056693+dexalium@users.noreply.github.com>
2025-04-17 15:27:23 +0700

    I GOT HOES CALLING A YOUNG MAN PHONE

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform target;
+
+    public Transform clampMin;
+    public Transform clampMax;
+
+    private Camera cam;
+    private float halfWidth;
+    private float halfHeight;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        target = FindAnyObjectByType<PlayerController>().transform;
+
+        clampMin.SetParent(null);
+        clampMax.SetParent(null);
+
+        cam = GetComponent<Camera>();
+        halfHeight = cam.orthographicSize;
+        halfWidth = cam.orthographicSize * cam.aspect;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        Vector3 clampedPosition = transform.position;
+
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, clampMin.position.x + halfWidth, clampMax.position.x - halfWidth);
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, clampMin.position.y + halfHeight, clampMax.position.y - halfHeight);
+
+        transform.position = clampedPosition;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,175 @@
+using Unity.Burst.Intrinsics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D RigidBody;
+    public Rigidbody2D theRB;
     public float moveSpeed;
 
-    public InputActionReference moveInput;
+    public InputActionReference moveInput, actionInput;
+    public Animator anim;
+
+    public enum ToolType
+    {
+        plough,
+        wateringCan,
+        seeds,
+        basket
+    }
+
+    public ToolType currentTool;
+
+    public float toolWaitTime = .5f;
+    private float toolWaitCounter;
+
+    public Transform toolIndicator;
+    public float toolRange = 3f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        UIController.instance.SwitchTool((int)currentTool);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //RigidBody.linearVelocity = new Vector2(moveSpeed, 0f);
-        RigidBody.linearVelocity = moveInput.action.ReadValue<Vector2>().normalized * moveSpeed;
+
+        if(toolWaitCounter > 0)
+        {
+            toolWaitCounter -= Time.deltaTime;
+            theRB.linearVelocity = Vector2.zero;
+        }
+        else
+    {
+            
+        //theRB.linearVelocity = new Vector2(moveSpeed, 0f);
+        theRB.linearVelocity = moveInput.action.ReadValue<Vector2>().normalized * moveSpeed;
+
+
+        if(theRB.linearVelocity.x < 0f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if(theRB.linearVelocity.x > 0f)
+        {
+            transform.localScale = Vector3.one;
+        }
     }
+
+        bool hasSwitchedTool = false;
+
+        if(Keyboard.current.tabKey.wasPressedThisFrame)
+        {
+            currentTool++;
+
+            if((int)currentTool >= 4)
+            {
+                currentTool = ToolType.plough;
+                
+            }
+            hasSwitchedTool = true;
+        }
+
+        if(Keyboard.current.digit1Key.wasPressedThisFrame)
+        {
+            currentTool = ToolType.plough;
+            hasSwitchedTool = true;
+        }
+        if(Keyboard.current.digit2Key.wasPressedThisFrame)
+        {
+            currentTool = ToolType.wateringCan;
+            hasSwitchedTool = true;
+        }
+        if(Keyboard.current.digit3Key.wasPressedThisFrame)
+        {
+            currentTool = ToolType.seeds;
+            hasSwitchedTool = true;
+        }
+        if(Keyboard.current.digit4Key.wasPressedThisFrame)
+        {
+            currentTool = ToolType.basket;
+            hasSwitchedTool = true;
+        }
+
+        if(hasSwitchedTool == true)
+        {
+            //FindFirstObjectByType<UIController>().SwitchTool((int)currentTool);
+            UIController.instance.SwitchTool((int)currentTool);
+        }
+       
+
+
+
+        if(actionInput.action.WasPressedThisFrame())
+        {
+            UseTool();
+        }
+
+
+        anim.SetFloat("speed", theRB.linearVelocity.magnitude);
+
+        toolIndicator.position = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        toolIndicator.position = new Vector3(toolIndicator.position.x, toolIndicator.position.y, 0f); 
+
+        if(Vector3.Distance(toolIndicator.position, transform.position) > toolRange)
+        {
+            Vector2 direction = toolIndicator.position - transform.position;
+            direction = direction.normalized * toolRange;
+            toolIndicator.position = transform.position + new Vector3(direction.x, direction.y, 0f);
+        }
+
+        toolIndicator.position = new Vector3(Mathf.FloorToInt(toolIndicator.position.x) + 0.5f,
+        Mathf.FloorToInt(toolIndicator.position.y) + 0.5f,
+        0f);
+    }
+
+    void UseTool()
+    {
+        GrowBlock block = null;
+
+        //block = FindFirstObjectByType<GrowBlock>();
+
+        //block.PloughSoil();
+
+        block = GridController.instance.GetBlock(toolIndicator.position.x, toolIndicator.position.y);
+
+        toolWaitCounter = toolWaitTime;
+
+        if(block != null)
+        {
+            switch(currentTool)
+            {
+                case ToolType.plough:
+                    
+                    block.PloughSoil();
+
+                    anim.SetTrigger("usePlough");
+
+                    break;
+
+                case ToolType.wateringCan:
+
+                    block.WaterSoil();
+
+                    anim.SetTrigger("useWateringcan");
+              
+                    break;
+                
+                case ToolType.seeds:
+
+                    block.PlantCrop();
+                    
+                    break;
+
+                case ToolType.basket:
+                
+                    block.HarvestCrop();
+
+                    break;
+            }
+        }
+    }
+
 }
