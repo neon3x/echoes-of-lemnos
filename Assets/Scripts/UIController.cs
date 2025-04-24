ae965148bc2b7e35b6515b131b7e15c0b085928c
neon3x <marvin.potu@gmail.com>
2025-04-24 15:07:10 +0700

    area switcher update but minus multiple area

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController instance;
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public GameObject[] toolbarActivatorIcons;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchTool(int selected)
+    {
+        foreach(GameObject icon in toolbarActivatorIcons)
+        {
+            icon.SetActive(false);
+        }
+
+        toolbarActivatorIcons[selected].SetActive(true);
+    }
+}
